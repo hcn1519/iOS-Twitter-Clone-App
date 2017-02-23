2de1cbe41727b75fc6721cb2a065479985d0dc5c
Hong, Chang Nam <hcn1519@gmail.com>
2017-02-23 15:22:30 +0900

    dynamic resizing cell finish

--- TwitterClone/HomeDataSourceController.swift
+++ TwitterClone/HomeDataSourceController.swift
@@ -15,6 +15,27 @@ class HomeDataSourceController: DatasourceController {
         self.datasource = homeDatasource
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        if let user = self.datasource?.item(indexPath) as? User {
+//            user.bioText
+            
+            let approximateWidthOfBioTextView = view.frame.width - 12 - 50 - 8 - 2
+            let size = CGSize(width: approximateWidthOfBioTextView, height: 1000)
+            let attribute = [NSFontAttributeName: UIFont.systemFont(ofSize: 15)]
+            
+            let estimatedFrame = NSString(string: user.bioText).boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: attribute, context: nil)
+            
+            return CGSize(width: view.frame.width, height: estimatedFrame.height + 66)
+        }
+        
+        
+        return CGSize(width: view.frame.width, height: 200)
+    }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: view.frame.width, height: 50)
     }
